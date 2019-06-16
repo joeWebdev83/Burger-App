142cd6d71e4df28ea3cd6ffbbda23a50811a440f
joel petion <joelpetion@yahoo.com>
2019-06-16 14:51:26 -0400

    added sql db and table

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	burger_name VARCHAR(100) not null,
+	devoured BOOLEAN default false
+	
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Stacked Veggie Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cripsy-Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Asian Style Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('All American Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plantain Burger', true);
